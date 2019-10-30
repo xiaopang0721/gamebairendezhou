cc6dddf80fb8034e71d83e12055ee885d1092406
xiaoer <xiaoer@example.com>
2019-10-30 19:45:16 +0800

    百人中途进场没有筹码

--- page/BairendezhouMapPage.ts
+++ page/BairendezhouMapPage.ts
@@ -645,6 +645,7 @@ module gamebairendezhou.page {
             chip.visible = false;
             this._chipTotalList[targetIdx - 1].push(chip);
             if (this._bairendezhouMgr.isReconnect && this._curStatus != MAP_STATUS.PLAY_STATUS_BET) {
+                chip.visible = true;
                 chip.drawChip();
             }
             else {
